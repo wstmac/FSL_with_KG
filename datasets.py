c076056841124c2e396c9e7c87f660e17fae0ed7
wstmac <wushuang81484@gmail.com>
2021-03-22 20:07:32 +0800

    add randomHorizontalFlip to transform

--- datasets.py
+++ datasets.py
@@ -46,6 +46,7 @@ class MiniImageNet(Dataset):
         self.transform = transforms.Compose([
             transforms.CenterCrop(224),
             transforms.Resize(84),
+            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225])
